3725c9d1f3142076cd55772116cc2c8d51b40dbe
John Morehouse <jmorehouse@TARDIS-MacBook-Pro.local>
2019-02-01 15:46:09 -0500

    adding demo scripts to repo
    adding in the demo scripts to the repo

---
+++ Azure Basics/CreateNewSQLVM.ps1
@@ -0,0 +1,69 @@
+﻿# Variables
+#https://docs.microsoft.com/en-us/azure/virtual-machines/windows/sql/virtual-machines-windows-ps-sql-create
+#install-module -name azurerm -AllowClobber -force
+#Import-Module -name azureRM
+
+#log into my Azure Account
+Login-AzureRmAccount
+
+## Global
+$Location = "EastUS"
+$ResourceGroupName = "SQL2017DevDemo"
+
+## Storage
+$StorageName = $ResourceGroupName.ToLower()
+$StorageSku = "Standard_LRS"  #locally redundant storage
+
+## Network
+$InterfaceName = $ResourceGroupName + "ServerInterface"
+$NsgName = $ResourceGroupName + "nsg"
+$VNetName = $ResourceGroupName + "VNet"
+$SubnetName = "Default"
+$VNetAddressPrefix = "10.0.0.0/16"
+$VNetSubnetAddressPrefix = "10.0.0.0/24"
+$TCPIPAllocationMethod = "Dynamic"
+
+##Compute
+$VMName = $ResourceGroupName + "VM"
+$ComputerName = "SQL2017DevDemo"
+$VMSize = "Standard_A5"
+$OSDiskName = $VMName + "OSDisk"
+
+##Image
+$PublisherName = "MicrosoftSQLServer"
+$OfferName = "SQL2017-WS2016"
+$Sku = "SQLDEV"
+$Version = "latest"
+
+# Resource Group
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Storage
+$StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -SkuName $StorageSku -Kind "Storage" -Location $Location
+
+# Network
+$SubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $VNetSubnetAddressPrefix
+$VNet = New-AzureRmVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VNetAddressPrefix -Subnet $SubnetConfig
+$PublicIp = New-AzureRmPublicIpAddress -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod $TCPIPAllocationMethod #-DomainNameLabel $DomainName
+$NsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig -Name "RDPRule" -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+$NsgRuleSQL = New-AzureRmNetworkSecurityRuleConfig -Name "MSSQLRule"  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 1433 -Access Allow
+$Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -NetworkSecurityGroupId $Nsg.Id -PublicIpAddressId $PublicIp.Id
+
+# Compute
+$VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+$Credential = Get-Credential -Message "Type the name and password of the local administrator account."
+$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate #-TimeZone = $TimeZone
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -Caching ReadOnly -CreateOption FromImage
+
+# Image
+$VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName $PublisherName -Offer $OfferName -Skus $Sku -Version $Version
+
+# Create the VM in Azure
+New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
+
+#Stop-AzureRmVM -Name $VMName -resourcegroupname $ResourceGroupName -force
+
+#remove-azurermresourcegroup -Name $ResourceGroupName -force
+ 
---
+++ Azure Basics/CreateNewSQLVM_CLI.ps1
@@ -0,0 +1,26 @@
+﻿
+#get the credentials from the user
+$creds = Get-Credential
+
+$resourceGroup = "CLIDemoPosh"az
+$srvName = "myCLIDemoPosh"
+
+az group create --name $resourceGroup --location eastus
+
+# use az vm image list -f --publisher MicrosoftSQLServer --all to find the list of images.  Use the URN value as the image name
+az vm create `
+    --resource-group $resourceGroup `
+    --name $srvName `
+    --image MicrosoftSQLServer:SQL2017-WS2016:SQLDEV:latest `
+    --admin-username $($creds.GetNetworkCredential().UserName) `
+    --admin-password $($creds.GetNetworkCredential().Password) `
+    --size Standard_A5 `
+    --storage-sku Standard_LRS
+
+
+#stop the VM
+#az vm stop --resource-group $ResourceGroup --name $srvName
+
+#destroy the VM
+#az group delete --name $ResourceGroup --yes
+
